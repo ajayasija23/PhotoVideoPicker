6351cc55e473aee104888f0974b1986f396b8380
Ajay <ajay@webcom>
2024-09-24 12:33:19 +0530

    code up

--- photopicker/build.gradle.kts
+++ photopicker/build.gradle.kts
@@ -49,9 +49,9 @@ publishing {
 
 dependencies {
 
-//    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-//    implementation(libs.material)
+    implementation(libs.material)
 //    testImplementation(libs.junit)
 //    androidTestImplementation(libs.androidx.junit)
 //    androidTestImplementation(libs.androidx.espresso.core)
