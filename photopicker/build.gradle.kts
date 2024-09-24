8fa4d43addca70531b59060d633d736ca876ea6b
Ajay <ajay@webcom>
2024-09-24 12:44:18 +0530

    code up

--- photopicker/build.gradle.kts
+++ photopicker/build.gradle.kts
@@ -38,7 +38,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.ajayasija23"
             artifactId = "photopicker"
-            version = "1.0.5"
+            version = "1.0.6"
 
             afterEvaluate {
                 from(components["release"])
